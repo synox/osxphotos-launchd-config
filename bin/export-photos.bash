a73144e9ab65336e5eb70751efab7cf19803aeed
Aravindo Wingeier <synox@users.noreply.github.com>
2021-01-06 00:53:41 +0100

    Update export-photos.bash

--- bin/export-photos.bash
+++ bin/export-photos.bash
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # pipx install osxphotos
- 
+
+# only need to trigger downloading on the first run
+
+# export by date and location
 /Users/aravindo/.local/bin/osxphotos \
-	export --update  --use-photokit  --strip  --touch-file  --deleted  --download-missing  \
+	export --update    --strip  --touch-file  --deleted --use-photokit --download-missing  \
 	--directory "{created.date} {place.name.city,} {place.country_code,}" \
+	--db "/Users/aravindo/Pictures/Photos Library.photoslibrary" \
+	"/Users/aravindo/Pictures/Apple-Photos-Exported"
+
+# export albums
+/Users/aravindo/.local/bin/osxphotos \
+	export --update  --touch-file  --deleted  \
+	--directory "{album}" \
 	--db "/Users/aravindo/Pictures/Photos Library.photoslibrary" \
 	"/Users/aravindo/Pictures/Apple-Photos-Exported"
