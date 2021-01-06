0ad773b9c345d1e0507a0f5c5aba83fc08c2b95d
Aravindo Wingeier <synox@users.noreply.github.com>
2021-01-06 01:21:30 +0100

    Update export-photos.bash

--- bin/export-photos.bash
+++ bin/export-photos.bash
@@ -2,18 +2,23 @@
 
 # pipx install osxphotos
 
-# only need to trigger downloading on the first run
+# only need to trigger downloading on the first run. Excluding deleted, as they can not be downloaded and otherwise we get warnings. 
+/Users/aravindo/.local/bin/osxphotos \
+	export --update    --strip  --touch-file --missing --download-missing  --use-photokit  \
+	--directory "{created.date} {place.name.city,} {place.country_code,}" \
+	--db "/Users/aravindo/Pictures/Photos Library.photoslibrary" \
+	"/Users/aravindo/Pictures/Apple-Photos-Exported"
 
 # export by date and location
 /Users/aravindo/.local/bin/osxphotos \
-	export --update    --strip  --touch-file  --deleted --use-photokit --download-missing  \
+	export --update  --strip  --touch-file  --deleted   \
 	--directory "{created.date} {place.name.city,} {place.country_code,}" \
 	--db "/Users/aravindo/Pictures/Photos Library.photoslibrary" \
 	"/Users/aravindo/Pictures/Apple-Photos-Exported"
 
 # export albums
 /Users/aravindo/.local/bin/osxphotos \
-	export --update  --touch-file  --deleted  \
+	export --update  --strip  --touch-file  --deleted   \
 	--directory "{album}" \
 	--db "/Users/aravindo/Pictures/Photos Library.photoslibrary" \
 	"/Users/aravindo/Pictures/Apple-Photos-Exported"
